c2ffd6f69deb21f63755a556a116730ad485012c
bluepsm <burapa9826@gmail.com>
2024-04-20 12:24:36 +0700

    Initialized Angular for the frontend app.

---
+++ joyty-angular/src/app/app-user/app-user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-user',
+  templateUrl: './app-user.component.html',
+  styleUrl: './app-user.component.css'
+})
+export class AppUserComponent {
+
+}
